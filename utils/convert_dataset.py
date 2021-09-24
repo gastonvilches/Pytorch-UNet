f88711d5e505d41e55b434e7a0810d19e99a57c0
gastonvilches <gastonvilches_96@hotmail.com>
2021-09-23 21:48:39 -0300

    Fixed errors in convert_dataset

--- utils/convert_dataset.py
+++ utils/convert_dataset.py
@@ -5,25 +5,25 @@ import numpy as np
 def convert_dataset(input_dir, output_dir, is_mask):
     if is_mask:
         # Masks processing
-        folders_list = os.listdir(input_masks_dir)
+        folders_list = os.listdir(input_dir)
         num_folders = len(folders_list)
         for idx, folder in enumerate(folders_list):
             print('Mask folder', idx, '/', num_folders)
-            for file in os.listdir(input_masks_dir + '/' + folder):
-                input_path = input_masks_dir + '/' + folder + '/' + file
-                output_path = output_masks_dir + '/' + folder + '_' + file
+            for file in os.listdir(input_dir + '/' + folder):
+                input_path = input_dir + '/' + folder + '/' + file
+                output_path = output_dir + '/' + folder + '_' + file
                 img = np.array(Image.open(input_path).convert(mode='L'))
                 img2 = Image.fromarray(255-img)
                 img2.save(output_path)
     else:
         # Images processing
-        folders_list = os.listdir(input_imgs_dir)
+        folders_list = os.listdir(input_dir)
         num_folders = len(folders_list)
         for idx, folder in enumerate(folders_list):
             print('Img folder', idx, '/', num_folders)
-            for file in os.listdir(input_imgs_dir + '/' + folder):
-                input_path = input_imgs_dir + '/' + folder + '/' + file
-                output_path = output_imgs_dir + '/' + folder + '_' + file
+            for file in os.listdir(input_dir + '/' + folder):
+                input_path = input_dir + '/' + folder + '/' + file
+                output_path = output_dir + '/' + folder + '_' + file
                 img = np.array(Image.open(input_path).convert(mode='L'))
                 img2 = np.zeros(img.shape, dtype=np.uint8)
                 img2[img < 255] = 255
